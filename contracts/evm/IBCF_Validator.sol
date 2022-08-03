59489494ea2ac41a6b797dfd18ebe68f99901c6c
Rodrigo Quelhas <r.quelhas@papers.ch>
2022-08-03 16:49:18 +0100

    feat(contracts): simplifies the validation of ethereum proofs and fixes the communication between client contracts

--- contracts/evm/IBCF_Validator.sol
+++ contracts/evm/IBCF_Validator.sol
@@ -85,7 +85,7 @@ contract IBCF_Validator {
     }
 
     /**
-     * Validate signatures
+     * Validates the 'merkle_root' authenticity
      */
     function validate_merkle_root(
         uint block_level,
@@ -105,32 +105,5 @@ contract IBCF_Validator {
 
     function validate_signature(bytes32 content_hash, address signer, uint[2] memory rs /* Signature */) internal view returns(bool) {
         return EllipticCurve.validateSignature(content_hash, rs, signer_public_key[signer]);
-    }
-
-    function getContent(uint block_level, bytes32 merkle_root) public view returns (bytes32) {
-        return sha256(abi.encodePacked(tezos_chain_id, block_level, merkle_root));
-    }
-}
-
-library Utils {
-    function string_of_uint(uint256 value) internal pure returns (string memory) {
-        // @credits https://github.com/OpenZeppelin/openzeppelin-contracts/blob/d50e608a4f0a74c75715258556e131a8e7e00f2d/contracts/utils/Strings.sol
-
-        if (value == 0) {
-            return "0";
-        }
-        uint256 temp = value;
-        uint256 digits;
-        while (temp != 0) {
-            digits++;
-            temp /= 10;
-        }
-        bytes memory buffer = new bytes(digits);
-        while (value != 0) {
-            digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
-            value /= 10;
-        }
-        return string(buffer);
     }
 }
--- contracts/tezos/IBCF_Client.py
+++ contracts/tezos/IBCF_Client.py
@@ -1,19 +1,23 @@
+# --------------------------------------------------------------------------
+# This contract is used only for demonstration purpuses.
+#
+# The contract is a minimal client example for interacting if the IBCF
+# protocol.
+# ---------------------------------------------------------------------------
 import smartpy as sp
 
 from contracts.tezos.IBCF_Aggregator import Type
 from contracts.tezos.IBCF_Eth_Validator import Type as ValidatiorInterface
 
-
 class Error:
     EXPECTING_PONG = "EXPECTING_PONG"
     EXPECTING_PING = "EXPECTING_PING"
+    INVALID_COUNTER = "INVALID_COUNTER"
 
 
-@staticmethod
 def string_of_nat(number):
     """Convert an int into a string"""
     c = sp.map({x: str(x) for x in range(0, 10)})
-    negative = number < 0
     x = sp.local("x", number)
     arr = sp.local("arr", [])
 
@@ -24,10 +28,45 @@ def string_of_nat(number):
         x.value //= 10
 
     result = sp.local("result", sp.concat(arr.value))
-    with sp.if_(negative):
-        result.value = "-" + result.value
 
     return result.value
+
+def nat_of_string(s):
+    """Convert a string into a int"""
+    c = sp.map({str(x): x for x in range(0, 10)})
+
+    text = sp.local("text", s)
+
+    result = sp.local("result", 0)
+    with sp.for_("idx", sp.range(0, sp.len(text.value))) as idx:
+        result.value = 10 * result.value + c[sp.slice(text.value, idx, 1).open_some()]
+
+    return result.value
+
+
+def bytes_of_string(text):
+    b = sp.pack(text)
+    # Remove (packed prefix), (Data identifier) and (string length)
+    # - Packed prefix: 0x05 (1 byte)
+    # - Data identifier: (string = 0x01) (1 byte)
+    # - String length (4 bytes)
+    return sp.slice(b, 6, sp.as_nat(sp.len(b) - 6)).open_some(
+        "Could not encode string to bytes."
+    )
+
+
+def ascii_string_of_bytes(b):
+    packedBytes = sp.concat(
+        [
+            sp.bytes("0x05"),
+            sp.bytes("0x01"),
+            sp.bytes("0x00000001"),
+            sp.slice(b, 1, 1).open_some(),
+        ]
+    )
+    return sp.unpack(packedBytes, sp.TString).open_some(
+        "Could not decode bytes to string"
+    )
 
 
 class IBCF_Client(sp.Contract):
@@ -39,8 +78,6 @@ class IBCF_Client(sp.Contract):
                 ibcf_tezos_state=sp.TAddress,
                 ibcf_eth_validator=sp.TAddress,
                 counter=sp.TNat,
-                latest_sent_payload=sp.TBytes,
-                latest_received_payload=sp.TBytes,
             )
         )
 
@@ -50,42 +87,50 @@ class IBCF_Client(sp.Contract):
 
     @sp.entry_point()
     def ping(self):
-        # Pings can only heappen when counter is even
+        # Pings can only happen when counter is even
         sp.verify(self.data.counter % 2 == 0, Error.EXPECTING_PONG)
 
         # Increase counter
         self.data.counter += 1
-        packed_counter = sp.compute(sp.pack(string_of_nat(self.data.counter)))
-        self.data.latest_sent_payload = packed_counter
+
+        packed_counter = sp.compute(bytes_of_string(string_of_nat(self.data.counter)))
 
         # Send ping
-        param = sp.record(key=sp.pack("counter"), value=packed_counter)
+        param = sp.record(key=bytes_of_string("counter"), value=packed_counter)
         contract = sp.contract(
             Type.InsertArgument, self.data.ibcf_tezos_state, "insert"
         ).open_some("INVALID_CONTRACT")
         sp.transfer(param, sp.mutez(0), contract)
 
-    @sp.entry_point()
+    @sp.entry_point(
+        parameter_type=sp.TRecord(block_number=sp.TNat, account_proof_rlp=sp.TBytes, storage_proof_rlp=sp.TBytes)
+    )
     def pong(self, param):
-        sp.set_type(param, sp.TRecord(block_number=sp.TNat, storage_proof=sp.TBytes))
-
         # Pongs can only happen when counter is odd
         sp.verify(self.data.counter % 2 == 1, Error.EXPECTING_PING)
 
         # Increase counter
         self.data.counter += 1
 
-        self.data.latest_received_payload = sp.view(
-            "get_storage",
+        response = sp.view(
+            "validate_proof",
             self.data.ibcf_eth_validator,
             sp.set_type_expr(
                 sp.record(
-                    account=self.data.eth_contract,
-                    storage_slot=self.data.storage_slot,
                     block_number=param.block_number,
-                    storage_proof=param.storage_proof,
+                    account=self.data.eth_contract,
+                    account_proof_rlp=param.account_proof_rlp,
+                    storage_slot=self.data.storage_slot,
+                    storage_proof_rlp=param.storage_proof_rlp,
                 ),
-                ValidatiorInterface.Get_storage_argument,
+                ValidatiorInterface.Validate_storage_proof_argument,
             ),
             t=sp.TBytes,
         ).open_some("INVALID_VIEW")
+
+        sp.verify(
+            ascii_string_of_bytes(response) == string_of_nat(self.data.counter),
+            (
+                Error.INVALID_COUNTER
+            ),
+        )
