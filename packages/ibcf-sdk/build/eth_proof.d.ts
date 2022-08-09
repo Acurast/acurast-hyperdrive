f8ced61eaf2e32ba9fbed677e5cdc4e642912aa7
Rodrigo Quelhas <r.quelhas@papers.ch>
2022-08-09 21:11:08 +0100

    chore: lint and pretify js code

--- packages/ibcf-sdk/build/eth_proof.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Eth } from 'web3-eth';
-export interface EthereumProof {
-    block_number: number;
-    account_proof_rlp: string;
-    storage_proof_rlp: string;
-}
-export declare function generateEthereumProof(web3_eth: Eth, address: string, slot: string, block_number: number): Promise<EthereumProof>;
