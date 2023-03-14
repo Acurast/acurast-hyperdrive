a2dbb583738841c0ed98b8bed9cbb305224ee4da
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2023-03-14 08:46:49 +0000

    feat: add job registration action and tests

--- compilation/tezos/AcurastProxy.py
+++ compilation/tezos/AcurastProxy.py
@@ -10,7 +10,16 @@ sp.add_compilation_target(
             governance_address=sp.address("KT18amZmM5W7qDWVt2pH6uj7sCEd3kbzLrHT"),
             merkle_aggregator=sp.address("KT18amZmM5W7qDWVt2pH6uj7sCEd3kbzLrHT"),
             proof_validator=sp.address("KT18amZmM5W7qDWVt2pH6uj7sCEd3kbzLrHT"),
-            authorized_actions=sp.big_map({"REGISTER_JOB": ActionLambda.register_job}),
+            authorized_actions=sp.big_map(
+                {
+                    "REGISTER_JOB": sp.record(
+                        function=ActionLambda.register_job,
+                        storage=sp.record(
+                            version=0, data=sp.pack(sp.record(job_id_seq=0))
+                        ),
+                    ),
+                }
+            ),
         ),
         outgoing_counter=0,
         registry=sp.big_map(),
